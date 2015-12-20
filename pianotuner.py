16d463c9a5850e75be07a6afee05c74bfb2a09b4
reinderien <gtoombs@lakeheadu.ca>
2015-12-20 14:01:03 -0500

    Initial commit - successfully captures and plots

---
+++ pianotuner.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+"""
+https://en.wikipedia.org/wiki/Piano#/media/File:Piano_Frequencies.svg
+88-key keyboard ranges from:
+    A0:   27.5Hz = 55 * 2^(-1)
+    A4:  440.0Hz = 55 * 2^( 3)
+    C8: 4186.0Hz = 55 * 2^( 6 + 3/12)
+"""
+
+import matplotlib, numpy, pprint, pyaudio, pyfftw, time
+from matplotlib import pyplot as plt
+
+
+def get_best_device(audio):
+    standard_rates = (192000, 96000, 88200, 48000, 44100, 32000, 24000, 22050, 16000, 12000, 11025, 9600, 8000)
+    default_device = audio.get_default_input_device_info()
+    best_rate, best_device = 0, default_device
+    for device_index in range(audio.get_device_count()):
+        device = audio.get_device_info_by_index(device_index)
+        if device['maxInputChannels'] < 1:
+            continue
+        device.update({'supported_rates': [],
+                       'is_default': device['index'] == default_device['index']})
+        for rate in standard_rates:
+            try:
+                if audio.is_format_supported(rate=rate, input_channels=1, input_format=pyaudio.paFloat32,
+                                             input_device=device['index']):
+                    device['supported_rates'].append(rate)
+                    if rate > best_rate or (rate == best_rate and device['is_default']):
+                        best_rate, best_device = rate, device
+            except ValueError:
+                pass
+
+    print('Best device:\n%s' % pprint.pformat(best_device))
+    print('Best rate: %d Hz' % best_rate)
+    return best_device, best_rate
+
+
+def plot(spectrum, t_window):
+    dx = [i/t_window for i in range(len(spectrum))]
+    dy = numpy.abs(spectrum)
+
+    fig, ax = plt.subplots(1, 1)
+    ax.set_title('Magnitude Spectrum')
+    ax.loglog(dx, dy)
+    ax.grid(which='major', axis='both', linestyle='-')
+    ax.grid(which='minor', axis='both')
+    ax.set_autoscaley_on(True)
+    ax.set_xticks(minor=False, ticks=[55 * pow(2, o) for o in range(-2, 8)])
+    ax.set_xticks(minor=True, ticks=[55 * pow(2, o + i / 12) for o in range(-2, 8) for i in range(1, 12)])
+    ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+    ax.set_xlim(xmin=55*pow(2, -2), xmax=55*pow(2, 8))
+
+    plt.show()
+
+
+def capture():
+    audio = pyaudio.PyAudio()
+    try:
+        best_device, best_rate = get_best_device(audio)
+        t_window = 1
+        n_frames = int(t_window * best_rate)
+
+        fft_in = pyfftw.n_byte_align_empty(n=16, shape=n_frames, dtype='float32')
+        fft_out = pyfftw.n_byte_align_empty(n=16, shape=n_frames // 2 + 1, dtype='complex64')
+        fftw = pyfftw.FFTW(fft_in, fft_out,
+                           direction='FFTW_FORWARD',
+                           flags=('FFTW_MEASURE', 'FFTW_DESTROY_INPUT'),
+                           threads=4, planning_timelimit=2)
+
+        def audio_cb(in_data, frame_count, time_info, status_flags):
+            in_data = pyfftw.n_byte_align(n=16, array=numpy.fromstring(in_data, dtype='float32'))
+            print('%d samples in [%f %f]; status %s' %
+                  (frame_count, min(in_data), max(in_data), status_flags))
+
+            fft_res = fftw(input_array=in_data)
+            peak_at = numpy.argmax(numpy.abs(fft_res))
+            print('Fundamental |%f| at %f Hz' % (numpy.abs(fft_res[peak_at]), peak_at / t_window))
+
+            if numpy.abs(fft_res[peak_at]) > 100:
+                flag = pyaudio.paComplete
+            else:
+                flag = pyaudio.paContinue
+
+            out_data = None
+            return out_data, flag
+
+        stream = audio.open(rate=best_rate, channels=1, format=pyaudio.paFloat32,
+                            input=True, input_device_index=best_device['index'],
+                            frames_per_buffer=n_frames, stream_callback=audio_cb)
+        try:
+            print('Capturing audio...')
+            while stream.is_active() and not stream.is_stopped():
+                time.sleep(1)
+            print('Closing audio...')
+        finally:
+            stream.stop_stream()
+            stream.close()
+    finally:
+        audio.terminate()
+
+    print('Plotting...')
+    plot(fft_out, t_window)
+
+
+print('Initializing...')
+capture()
+print('Complete.')
