5120dcca52f34967d725a88ff505e38756dce0ad
reinderien <gtoombs@lakeheadu.ca>
2015-12-20 16:45:54 -0500

    Move capture callback out to top level

--- pianotuner.py
+++ pianotuner.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
 
-"""
-https://en.wikipedia.org/wiki/Piano#/media/File:Piano_Frequencies.svg
-88-key keyboard ranges from:
-    A0:   27.5Hz = 55 * 2^(-1)
-    A4:  440.0Hz = 55 * 2^( 3)
-    C8: 4186.0Hz = 55 * 2^( 6 + 3/12)
-"""
 
 import math, matplotlib, numpy, pprint, pyaudio, pyfftw, scipy.interpolate, time
 from matplotlib import pyplot as plt
@@ -38,7 +31,7 @@ def get_best_device(audio):
 
 
 def plot(spectrum, t_window):
-    dx = [i/t_window for i in range(len(spectrum))]
+    dx = [i / t_window for i in range(len(spectrum))]
     dy = numpy.abs(spectrum)
     dxnew = [55 * pow(2, o + i / 48) for o in range(-2, 8) for i in range(1, 48)]
     dynew = scipy.interpolate.interp1d(dx, dy)(dxnew)
@@ -52,12 +45,13 @@ def plot(spectrum, t_window):
     ax.set_xticks(minor=False, ticks=[55 * pow(2, o) for o in range(-2, 8)])
     ax.set_xticks(minor=True, ticks=[55 * pow(2, o + i / 12) for o in range(-2, 8) for i in range(1, 12)])
     ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-    ax.set_xlim(xmin=55*pow(2, -2), xmax=55*pow(2, 8))
+    ax.set_xlim(xmin=55 * pow(2, -2), xmax=55 * pow(2, 8))
 
     plt.show()
 
 
 def get_note_name(f):
+    # https://en.wikipedia.org/wiki/Piano#/media/File:Piano_Frequencies.svg
     names = ('C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B')
     n = math.floor(12 * math.log(f / 55) / math.log(2) + 0.5) + 21
     name = names[n % 12] + str(int(n / 12))
@@ -74,46 +68,51 @@ def get_note(spectrum, t_window):
     return f, n, name, v
 
 
+def audio_cb(fftw, t_window):
+    def inner(in_data, frame_count, time_info, status_flags):
+        in_data = pyfftw.n_byte_align(n=16, array=numpy.fromstring(in_data, dtype='float32'))
+        print('%d samples in [%f %f]; status %s' %
+              (frame_count, min(in_data), max(in_data), status_flags))
+
+        fft_res = fftw(input_array=in_data)
+        note = get_note(fft_res, t_window)
+        if note:
+            f, n, name, v = note
+            print('Fundamental |%.1f| at %.1f Hz, n=%d "%s"' % (v, f, n, name))
+            flag = pyaudio.paComplete
+        else:
+            flag = pyaudio.paContinue
+
+        out_data = None
+        return out_data, flag
+    return inner
+
+
 def capture():
+    print('Initializing audio...')
     audio = pyaudio.PyAudio()
     try:
+        print('Selecting audio device...')
         best_device, best_rate = get_best_device(audio)
         t_window = 1
         n_frames = int(t_window * best_rate)
 
+        print('Initializing FFT...')
         fft_in = pyfftw.n_byte_align_empty(n=16, shape=n_frames, dtype='float32')
         fft_out = pyfftw.n_byte_align_empty(n=16, shape=n_frames // 2 + 1, dtype='complex64')
         fftw = pyfftw.FFTW(fft_in, fft_out,
                            direction='FFTW_FORWARD',
                            flags=('FFTW_MEASURE', 'FFTW_DESTROY_INPUT'),
                            threads=4, planning_timelimit=2)
-
-        def audio_cb(in_data, frame_count, time_info, status_flags):
-            in_data = pyfftw.n_byte_align(n=16, array=numpy.fromstring(in_data, dtype='float32'))
-            print('%d samples in [%f %f]; status %s' %
-                  (frame_count, min(in_data), max(in_data), status_flags))
-
-            fft_res = fftw(input_array=in_data)
-            note = get_note(fft_res, t_window)
-            if note:
-                f, n, name, v = note
-                print('Fundamental |%f| at %f Hz, n=%d "%s"' % (v, f, n, name))
-                flag = pyaudio.paComplete
-            else:
-                flag = pyaudio.paContinue
-
-            out_data = None
-            return out_data, flag
-
+        print('Opening capture stream...')
         stream = audio.open(rate=best_rate, channels=1, format=pyaudio.paFloat32,
                             input=True, input_device_index=best_device['index'],
-                            frames_per_buffer=n_frames, stream_callback=audio_cb)
+                            frames_per_buffer=n_frames, stream_callback=audio_cb(fftw, t_window))
         try:
-            print('Capturing audio...')
             while stream.is_active() and not stream.is_stopped():
                 time.sleep(1)
-            print('Closing audio...')
         finally:
+            print('Closing audio...')
             stream.stop_stream()
             stream.close()
     finally:
@@ -123,6 +122,5 @@ def capture():
     plot(fft_out, t_window)
 
 
-print('Initializing...')
 capture()
 print('Complete.')
