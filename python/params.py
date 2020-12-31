8032bef6447e6ba53c12d614eb30bdcd2fd74f48
gtoombs <gtoombs@encircleapp.com>
2020-12-31 17:28:07 -0500

    Refactoring pass; currently segfaults in matplotlib draw

--- python/params.py
+++ python/params.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 
-from math import *
+from math import ceil, log
 
 
-def mu(note):
+def mu(note: int) -> float:
     return 2**(note/12)
 
 
+def mu_inv(freq: float) -> float:
+    return 12 * log(freq) / LOG_2
+
+
+LOG_2 = log(2)
 n_notes = 88
 f_a4 = 440                      # cycles/sec
-a4_index = 4*12                 # notes
+a4_index = 4*12                 # semitones
 f_samp = 48_000                 # samples/sec
 f_min = f_a4 * mu(-a4_index)    # cycles/sec
 f_max = f_min * mu(n_notes - 1) # cycles/sec
@@ -21,18 +26,22 @@ samp_min = t_min*f_samp         # samples/cycle
 samp_act = 2**ceil(log(samp_min)/log(2))  # samples/cycle
 t_act = samp_act / f_samp       # secs/cycle
 f_lower = 1/t_act               # cycles/sec
-lower_index = 12 * log(f_lower/f_min) / log(2)
+lower_index = mu_inv(f_lower/f_min)  # semitones
 
-print(f't_min = {t_min*1e3:.1f}ms')
-print(f't_act = {t_act*1e3:.1f}ms')
-print(f'Min samples = {samp_min:.0f}')
-print(f'Act samples = {samp_act}')
-print(f'Min detectable index = {lower_index:.1f}')
-print(f'Min detectable freq = {f_lower:.3f} (also spectral resolution)')
-print(f'f_mintune = {f_min_tune:.3f}')
-print(f'f_min = {f_min:.3f}')
-print(f'f_max = {f_max:.1f}')
-print(f'f_maxtune = {f_max_tune:.1f}')
-print(f'Max detectable freq = {f_upper}')
-print(f'Max detectable harmonic = {f_upper/f_max_tune:.1f}')
 
+def dump():
+    print(f't_min = {t_min*1e3:.1f}ms')
+    print(f't_act = {t_act*1e3:.1f}ms')
+    print(f'Min samples = {samp_min:.0f}')
+    print(f'Act samples = {samp_act}')
+    print(f'Min detectable index = {lower_index:.1f} semitones')
+    print(f'Min detectable freq = {f_lower:.3f} Hz (also spectral resolution)')
+    print(f'f_mintune = {f_min_tune:.3f} Hz')
+    print(f'f_min = {f_min:.3f} Hz')
+    print(f'f_max = {f_max/1e3:.3f} kHz')
+    print(f'f_maxtune = {f_max_tune/1e3:.3f} kHz')
+    print(f'Max detectable freq = {f_upper/1e3} kHz')
+    print(f'Max detectable harmonic = {f_upper/f_max_tune:.1f}')
+
+
+dump()
--- python/pianotuner.py
+++ python/pianotuner.py
@@ -1,52 +1,43 @@
 #!/usr/bin/env python3
+import math
+import matplotlib
+import numpy as np
+import pyaudio
+import scipy.interpolate
+from contextlib import contextmanager
+from pprint import pprint
+from typing import Callable, Dict, Literal, Optional, Tuple, Union
 
-
-import math, matplotlib, numpy, pprint, pyaudio, pyfftw, scipy.interpolate
+import pyfftw
 from matplotlib import pyplot as plt
+from pyfftw.pyfftw import FFTW
+
+RATE = 44_100
+
+DeviceDict = Dict[str, Union[str, int, float]]
 
 
-def get_best_device(audio):
-    standard_rates = (192000, 96000, 88200, 48000, 44100, 32000, 24000, 22050, 16000, 12000, 11025, 9600, 8000)
-    default_device = audio.get_default_input_device_info()
-    best_rate, best_device = 0, default_device
-    for device_index in range(audio.get_device_count()):
-        device = audio.get_device_info_by_index(device_index)
-        if device['maxInputChannels'] < 1:
-            continue
-        device.update({'supported_rates': [],
-                       'is_default': device['index'] == default_device['index']})
-        for rate in standard_rates:
-            try:
-                if audio.is_format_supported(rate=rate, input_channels=1, input_format=pyaudio.paFloat32,
-                                             input_device=device['index']):
-                    device['supported_rates'].append(rate)
-                    if rate > best_rate or (rate == best_rate and device['is_default']):
-                        best_rate, best_device = rate, device
-            except ValueError:
-                pass
-
-    print('Best device:\n%s' % pprint.pformat(best_device))
-    print('Best rate: %d Hz' % best_rate)
-    return best_device, best_rate
-
-
-def get_fund_index(spectrum):
-    mags = numpy.abs(spectrum)
+def get_fund_index(spectrum: np.ndarray) -> int:
+    mags = np.abs(spectrum)
     try:
         first_i = next(i for i, v in enumerate(mags) if v >= 100)
     except StopIteration:
-        first_i = numpy.argmax(mags)
-        print('Nothing above |100|; using index %d' % first_i)
+        first_i = np.argmax(mags)
+        print(f'Nothing above |100|; using index {first_i}')
         return first_i
 
     # Look for the largest component between here and the approximate midpoint to the
     # next harmonic
-    best_i = first_i + numpy.argmax(mags[first_i: int(first_i * 1.5)])
-    print('Best in [%d, %d] is %d' % (first_i, first_i * 1.5, best_i))
+    best_i = first_i + np.argmax(mags[first_i: int(first_i * 1.5)])
+    print(f'Best in [{first_i}, {first_i*1.5}] is {best_i}')
     return best_i
 
 
-def plot_init(t_window):
+def init_plot(t_window: int) -> Tuple[
+    Callable[[], None],  # plot_loop
+    Callable[[np.ndarray], None],  # plot
+]:
+    print('Initializing plot...')
     fig, (ax1, ax2) = plt.subplots(ncols=2, sharey=True)
 
     ax1.set_title('Broad Spectrum')
@@ -73,9 +64,9 @@ def plot_init(t_window):
     def plot_loop():
         plt.show(block=True)
 
-    def plot(spectrum):
+    def plot(spectrum: np.ndarray):
         broad_x = [i / t_window for i in range(len(spectrum))]
-        broad_y = numpy.abs(spectrum)
+        broad_y = np.abs(spectrum)
         broad_x_new = [55 * pow(2, o + i / 48) for o in range(-2, 8) for i in range(1, 48)]
         broad_y_new = scipy.interpolate.interp1d(broad_x, broad_y)(broad_x_new)
         broad_line.set_xdata(broad_x_new)
@@ -85,52 +76,61 @@ def plot_init(t_window):
         fund_f = fund_i / t_window
         fund_n = f_to_n(fund_f)
         fund_n_goal = round(fund_n)
-        print('fund i %d f %f n %f -> %d' % (
-            fund_i, fund_f, fund_n, fund_n_goal
-        ))
+        print(f'fund i={fund_i} f={fund_f:.1f} n={fund_n:.1f} -> {fund_n_goal}')
         lower_n, higher_n = fund_n_goal - 1, fund_n_goal + 1
         lower_f, higher_f = n_to_f(lower_n), n_to_f(higher_n)
         lower_i, higher_i = int(math.floor(lower_f * t_window)), int(math.ceil(higher_f * t_window))
         tune_x = [100*(f_to_n(i / t_window) - fund_n_goal)
                   for i in range(lower_i, higher_i)]
         tune_y = broad_y[lower_i: higher_i]
-        print('n %f-%f f %f-%f i %f-%f tune-x %f-%f' % (
-            lower_n, higher_n,
-            lower_f, higher_f,
-            lower_i, higher_i,
-            tune_x[0], tune_x[-1]
-
-        ) )
+        print(
+            f'n {lower_n}-{higher_n} '
+            f'f {lower_f:.1f}-{higher_f:.1f} '
+            f'i {lower_i}-{higher_i} '
+            f'tune-x {tune_x[0]:.1f}-{tune_x[-1]:.1f}'
+        )
         tune_x_new = list(range(-100, 101))
         tune_y_new = scipy.interpolate.interp1d(tune_x, tune_y, bounds_error=False)(tune_x_new)
         tune_line.set_xdata(tune_x_new)
         tune_line.set_ydata(tune_y_new)
-        ax2.set_xlabel('Tune (cents) from %s=%fHz' % (n_to_name(fund_n_goal), n_to_f(fund_n_goal)))
+        ax2.set_xlabel(
+            f'Tune (cents) from {n_to_name(fund_n_goal)}'
+            f'={n_to_f(fund_n_goal)}Hz'
+        )
 
+        print('Drawing...')
         fig.canvas.draw()
         fig.canvas.flush_events()
+        print('Done drawing.')
 
     return plot_loop, plot
 
 
-def n_to_f(n):
-    return 55*pow(2, (n - 21)/12)
+def n_to_f(n: float) -> float:
+    return 55 * 2**((n - 21)/12)
 
 
-def f_to_n(f):
-    return 12*math.log(f / 55)/math.log(2) + 21
+def f_to_n(f: float) -> float:
+    return 12 * math.log(f / 55)/math.log(2) + 21
 
 
-def n_to_name(n):
-    # https://en.wikipedia.org/wiki/Piano#/media/File:Piano_Frequencies.svg
-    names = ('C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B')
-    n = math.floor(n + 0.5)
-    return names[n % 12] + str(int(n / 12))
+# https://en.wikipedia.org/wiki/Piano#/media/File:Piano_Frequencies.svg
+NAMES = ('C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B')
 
 
-def get_note(spectrum, t_window):
+def n_to_name(n: float) -> str:
+    n = int(math.floor(n + 0.5))
+    return NAMES[n % 12] + str(int(n / 12))
+
+
+def get_note(spectrum: np.ndarray, t_window: float) -> Optional[Tuple[
+    float,  # freq
+    float,  # note index
+    str,    # note name
+    float,  # fundamental
+]]:
     try:
-        i, v = next((i, v) for i, v in enumerate(numpy.abs(spectrum)) if v >= 100)
+        i, v = next((i, v) for i, v in enumerate(np.abs(spectrum)) if v >= 100)
     except StopIteration:
         return None
     f = i / t_window
@@ -139,53 +139,85 @@ def get_note(spectrum, t_window):
     return f, n, name, v
 
 
-def audio_cb(fftw, t_window, plot):
-    def cb(in_data, frame_count, time_info, status_flags):
-        in_data = pyfftw.n_byte_align(n=16, array=numpy.fromstring(in_data, dtype='float32'))
-        print('%d samples in [%f %f]; status %s' %
-              (frame_count, min(in_data), max(in_data), status_flags))
+CallbackResult = Tuple[
+    Optional[bytes],
+    Literal[pyaudio.paContinue, pyaudio.paComplete],
+]
+AudioCallback = Callable[
+    [
+        bytes,  # in_data
+        int,    # frame_count
+        dict,   # time_info
+        int,    # status_flags
+    ],
+    CallbackResult
+]
 
-        fft_res = fftw(input_array=in_data)
+
+def audio_cb(
+    fftw: FFTW,
+    t_window: float,
+    plot: Callable[[np.ndarray], None],
+) -> AudioCallback:
+
+    def cb(in_data: bytes, frame_count: int, time_info: dict, status_flags: int) -> Tuple[
+        Optional[bytes],
+        Literal[pyaudio.paContinue, pyaudio.paComplete],
+    ]:
+        print(
+            f'{len(in_data)} elements of {type(in_data[0])}, '
+            f'{frame_count} samples, '
+            f'status {status_flags}'
+        )
+        in_data = pyfftw.byte_align(
+            array=np.frombuffer(in_data, dtype=np.float32)
+        )
+        print(f'Aligned to {in_data.shape} {in_data.dtype}, '
+              f'in [{min(in_data):.2f} {max(in_data):.2f}]')
+
+        fft_res: np.ndarray = fftw(input_array=in_data)
         note = get_note(fft_res, t_window)
         if note:
             f, n, name, v = note
-            print('Fundamental |%.1f| at %.1f Hz, n=%d "%s"' % (v, f, n, name))
+            print(f'Fundamental |{v:.1f}| at {f:.1f} Hz, n={n:.1f} "{name}"')
+
         try:
             plot(fft_res)
             flag = pyaudio.paContinue
-        except RuntimeError:
+        except RuntimeError as e:
+            print(e)
             flag = pyaudio.paComplete
+
         out_data = None
         return out_data, flag
+
     return cb
 
 
-def capture():
+@contextmanager
+def init_audio(n_frames: int, cb: Callable):
     print('Initializing audio...')
     audio = pyaudio.PyAudio()
+
     try:
-        print('Selecting audio device...')
-        best_device, best_rate = get_best_device(audio)
-        t_window = 1
-        n_frames = int(t_window * best_rate)
-
-        print('Initializing FFT...')
-        fft_in = pyfftw.n_byte_align_empty(n=16, shape=n_frames, dtype='float32')
-        fft_out = pyfftw.n_byte_align_empty(n=16, shape=n_frames // 2 + 1, dtype='complex64')
-        fftw = pyfftw.FFTW(fft_in, fft_out,
-                           direction='FFTW_FORWARD',
-                           flags=('FFTW_MEASURE', 'FFTW_DESTROY_INPUT'),
-                           threads=4, planning_timelimit=2)
-
-        print('Initializing plot...')
-        plot_loop, plot = plot_init(t_window)
+        print('Selecting default audio device...', end=' ')
+        device = audio.get_default_input_device_info()
+        print(f"#{device['index']} @ {RATE}")
 
         print('Opening capture stream...')
-        stream = audio.open(rate=best_rate, channels=1, format=pyaudio.paFloat32,
-                            input=True, input_device_index=best_device['index'],
-                            frames_per_buffer=n_frames, stream_callback=audio_cb(fftw, t_window, plot))
+        stream = audio.open(
+            input_device_index=device['index'],
+            input=True,
+            rate=RATE,
+            channels=1,
+            format=pyaudio.paFloat32,
+            frames_per_buffer=n_frames,
+            stream_callback=cb,
+        )
+        print()
+
         try:
-            plot_loop()
+            yield
         finally:
             print('Closing audio...')
             stream.stop_stream()
@@ -194,5 +226,37 @@ def capture():
         audio.terminate()
 
 
-capture()
+def plan_window() -> Tuple[
+    int,    # samples
+    float,  # window duration
+]:
+    t_window_approx = 1
+    n_frames = 2**int(math.log(RATE / t_window_approx) / math.log(2))
+    t_window = n_frames / RATE
+    return n_frames, t_window
+
+
+def init_fft(n_frames: int) -> FFTW:
+    print('Initializing FFT...')
+    fft_in = pyfftw.empty_aligned(shape=n_frames, dtype='float32')
+    fft_out = pyfftw.empty_aligned(shape=n_frames // 2 + 1, dtype='complex64')
+    return pyfftw.FFTW(
+        fft_in, fft_out,
+        direction='FFTW_FORWARD',
+        flags=('FFTW_MEASURE', 'FFTW_DESTROY_INPUT'),
+        threads=4,
+        planning_timelimit=2,
+    )
+
+
+def main():
+    n_frames, t_window = plan_window()
+    fftw = init_fft(n_frames)
+    plot_loop, plot = init_plot(t_window)
+
+    with init_audio(n_frames, audio_cb(fftw, t_window, plot)):
+        plot_loop()
+
+
+main()
 print('Complete.')
